9ec31cb6330774b984f4e947030dcc5d0d4ebcea
pelumi-ola <ola.e.pelumi@gmail.com>
2025-09-26 09:35:57 +0100

    Update favicon title url header

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,6 +35,15 @@ export const metadata = {
     follow: true,
   },
   metadataBase: new URL("https://streamd.ng"),
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", sizes: "48x48", type: "image/x-icon" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
